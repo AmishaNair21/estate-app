62de35ae906646d8ff8d6516c68a291837281f48
AmishaNair21 <amishamnair2002@gmail.com>
2024-03-18 15:13:48 +0530

    database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 const app=express();
+import dotenv from "dotenv"
 
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongo");
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 app.listen(3000,()=>{
     console.log("server runingg!")
